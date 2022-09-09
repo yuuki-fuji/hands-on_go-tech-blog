2218a11ffc02f589b55923f069e660cfc6f1e3f5
fujimoto <yuukikun666@gmail.com>
2022-09-10 01:32:45 +0900

    done

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/flosch/pongo2"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+const tmplPath = "src/template/"
+
+var e = createMux()
+
+func main() {
+	// '/'というパス（URL）と'articleIndex'という処理を結びつける
+	e.GET("/", articleIndex)
+	e.GET("/new", articleNew)
+	e.GET("/:id", articleShow)
+	e.GET("/:id/edit", articleEdit)
+
+	// Webサーバーをポート番号8080で起動する
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func createMux() *echo.Echo {
+	// アプリケーションインスタンスを生成
+	e := echo.New()
+
+	// アプリケーションに各種ミドルウェアを設定
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+
+	e.Static("/css", "src/css")
+	e.Static("/js", "src/js")
+
+	// アプリケーションインスタンスを返却
+	return e
+}
+
+func articleIndex(c echo.Context) error {
+	data := map[string]interface{}{
+		"Message": "Article Index",
+		"Now":     time.Now(),
+	}
+	return render(c, "article/index.html", data)
+}
+
+func articleNew(c echo.Context) error {
+	data := map[string]interface{}{
+		"Message": "Article New",
+		"Now":     time.Now(),
+	}
+
+	return render(c, "article/new.html", data)
+}
+
+func articleShow(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	data := map[string]interface{}{
+		"Message": "Article Show",
+		"Now":     time.Now(),
+		"ID":      id,
+	}
+	return render(c, "article/show.html", data)
+}
+
+func articleEdit(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	data := map[string]interface{}{
+		"Message": "Article Edit",
+		"Now":     time.Now(),
+		"ID":      id,
+	}
+	return render(c, "article/edit.html", data)
+}
+
+func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
+}
+
+func render(c echo.Context, file string, data map[string]interface{}) error {
+	b, err := htmlBlob(file, data)
+	if err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.HTMLBlob(http.StatusOK, b)
+}
